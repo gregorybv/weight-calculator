9ff761e9a495504381f7e8931bf78b25e3f5d2db
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-02 10:19:30 +0300

    add style to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+
+import './index.css'
 
 function App() {
+
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+
+  let imgSrc = ''
+
   return (
     <div className="app">
+      <div className="container">
+        <div className="center">BMI Calculator</div>
+        <form>
+          <div>
+            <label>Weight(lbs)</label>
+            <input value={weight}/>
+          </div>
+          <div>
+            <label>Height (in)</label>
+            <input value={height}/>
+          </div>
+          <div>
+            <button className="btn" type='submit'>Submit</button>
+            <button className="btn btn-outline" type='submit'>Submit</button>
+          </div>
+        </form>
+
+        <div className='center'>
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+
+        <div className='img-container'>
+          <img src={imgSrc} alt="/"/>
+        </div>
+      </div>
     </div>
   );
 }
